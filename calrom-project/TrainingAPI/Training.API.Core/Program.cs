953318fcec074e729d5042e7ca71d63192c1a7ab
lrobt97 <69146855+lrobt97@users.noreply.github.com>
2021-01-29 14:05:01 +0000

    #3 add project structure + stylecop + fxcop

---
+++ calrom-project/TrainingAPI/Training.API.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Training.API.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
