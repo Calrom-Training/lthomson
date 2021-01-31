1d3d6bf3cf549f89f8851e8f880c02f34331262c
lrobt97 <69146855+lrobt97@users.noreply.github.com>
2021-01-31 12:09:03 +0000

    #3 project restructure to use web API template

---
+++ calrom-project/TrainingAPI/Training.API.Core/IServices/IMessagingService.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IMessagingService.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace Training.API.Core.IServices
+{
+    using System.Collections.Generic;
+    using Training.API.Core.Models;
+
+    /// <summary>
+    ///  Messaging service interface.
+    /// </summary>
+    public interface IMessagingService
+    {
+        /// <summary>Gets the messages.</summary>
+        /// <param name="username">The username.</param>
+        /// <returns>
+        ///  A list of messages for the user.
+        /// </returns>
+        List<Message> GetMessages(string username);
+    }
+}
